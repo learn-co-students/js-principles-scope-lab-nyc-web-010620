89a3e1c15d674ba585587500077743fc70fecd42
github username <github email address>
2020-02-15 23:48:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+customerName='bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+      bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Assignment to constant variable.'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'unsuccessfully tries to reassign the least favorite customer';
+}
+
+
+
+
+   
